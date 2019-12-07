d3571b7312ace404bda245b74b58f7d46a1a96cd
Bob Cook <bob.cook.1966@gmail.com>
2019-12-07 15:33:58 +0000

    ts example

--- cs/example.cs
+++ cs/example.cs
@@ -1,7 +1,26 @@
+class Vehicle {
+}
 
-class AccountCode {    
-    public string _stringValue;
-    public AccountCode(string s) {
-        _stringValue = s;
-    }
+class Car : Vehicle{
 };
+
+class Plane : Vehicle{
+};
+
+interface CarPark {
+    void park(Car car);
+}
+interface PlanePark {
+    void park(Plane plane);
+}
+
+class BigPark : CarPark, PlanePark {
+    void park(Vehicle vehicle){
+        System.Console.WriteLine("park",vehicle);
+    }
+}
+
+CarPark carPark = new BigPark();
+PlanePark planePark = new BigPark();
+carPark.park(new Car());
+planePark.park(new Plane());
---
+++ ts/example.js
@@ -0,0 +1,45 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+var Vehicle = /** @class */ (function () {
+    function Vehicle() {
+    }
+    return Vehicle;
+}());
+var Car = /** @class */ (function (_super) {
+    __extends(Car, _super);
+    function Car() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    return Car;
+}(Vehicle));
+;
+var Plane = /** @class */ (function (_super) {
+    __extends(Plane, _super);
+    function Plane() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    return Plane;
+}(Vehicle));
+;
+var BigPark = /** @class */ (function () {
+    function BigPark() {
+    }
+    BigPark.prototype.park = function (vehicle) {
+        console.log("park " + vehicle.constructor);
+    };
+    return BigPark;
+}());
+var park = new BigPark();
+park.park(new Car());
+park.park(new Plane());
